40c7d69315161de326aaa1f7f4975781955e98b2
Alphamineron <alphamineron@gmail.com>
2019-07-02 23:48:44 +0530

    Attempting to use msgpack for data packaging

--- dataChip.py
+++ dataChip.py
@@ -41,6 +41,7 @@ class Todo(object):
         self.title = ""
         self.note = ""
         self.status = False
+        pass
 
 
 class TodoSection(object):
@@ -55,6 +56,13 @@ class TodoSection(object):
         self.list = []      # List of Todo Objects IDs
         pass
 
+"""
+    ID : Unique ID of the Chip Object
+    img : Web Scraped image related to the chip, extacted from the url
+
+
+"""
+
 class Chip(Primitive_Bookmark):
     """
         docstring for Chip
@@ -63,13 +71,68 @@ class Chip(Primitive_Bookmark):
     def __init__(self):
         super()
         self.ID = None          # NoneID invalidates the chip, equivalent to deleting it
-        self.title = self.name
         self.description = ""
         self.img = None         # LFS
         self.cache = None       # LFS
+        self.TodoList = []   # List of TodoSections Objects IDs
+
+        self.source = []        # Is it from Medium? Is it from Youtube
+        self.kind = []          # Is this a video, article, or image? What kind of content this represents
         self.tags = []          # List of Strings
         self.useCases = []      # List of UseCase Objects IDs
+        self.topics = []        # List of Topics that the Chip's contents fall into
         self.phaseID = None     # Color Hex Value
-        self.status = None      # 4-State Value - Done - In_Progress - ToDo - None(N/A)
-        self.TodoList = False   # List of TodoSections Objects IDs
-        pass
+        self.status = None      # 4-State Value: Done - In_Progress - ToDo - None(N/A)
+
+        self.collectionIDs = None
+        self.folderID = None
+
+    def inherit(self, p_bm):
+        self.name = p_bm.name
+        self.url = p_bm.url
+        self.time = p_bm.time
+
+    @staticmethod
+    def iterChip(*chipObjs):
+        for chip in chipObjs:
+            yield chip
+
+def test_initPrimitiveBookmarks(limit):
+    for i in range(limit):
+        p_bm = Primitive_Bookmark()
+        p_bm.name = "Test_" + str(i)
+        p_bm.url = "https://www.test" + str(i) + ".com"
+        p_bm.time = "DateTimeObject_" + str(i)
+        yield p_bm
+
+def main():
+
+    for p_bm in test_initPrimitiveBookmarks(1000):
+        chip = Chip()
+        chip.inherit(p_bm)
+        chip.show()
+
+
+
+if __name__ == '__main__':
+    import msgpack
+
+    # Define data
+    data = {'a list': [1, 42, 3.141, 1337, 'help'],
+            'a string': 'bla',
+            'another dict': {'foo': 'bar',
+                             'key': 'value',
+                             'the answer': 42}}
+
+    # Write msgpack file
+    with open('data.msgpack', 'wb') as outfile:
+        msgpack.packb(data, outfile)
+
+    # Read msgpack file
+    with open('data.msgpack', "rb") as data_file:
+        # data_loaded = json.load(data_file)
+        data_loaded = msgpack.unpack(data_file)
+
+    print(data)
+    print(data_loaded)
+    # main()
