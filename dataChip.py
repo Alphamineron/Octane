358a6cb5df671e16056f6b7e96fdbb21be772a0a
Alphamineron <alphamineron@gmail.com>
2019-07-14 15:38:46 +0530

    Trying out UUID, minor code rearragement, serialization support

--- dataChip.py
+++ dataChip.py
@@ -1,6 +1,13 @@
+import uuid
+import json
+
+from ChipOps.dfh import *
+
 from colorama import init, Fore
 import colorama
+import pprint
 
+USERCODE = "0000"
 PROJECT_NAME = "BMM"
 
 class Primitive_Bookmark(object):
@@ -23,15 +30,118 @@ class Primitive_Bookmark(object):
         self.time = ""
 
 
+class Chip(Primitive_Bookmark):
+    """
+        docstring for Chip
+        ID Format: C<USERCODE>XXXXX
+    """
+    def __init__(self, p_bm = None):
+        super()
+        if p_bm:
+            self.inherit(p_bm)
+
+        self.ID = "C" + USERCODE + str(uuid.uuid4()).replace("-", "")       # NoneID invalidates the chip, equivalent to deleting it
+        self.description = ""
+        self.img = None         # LFS
+        self.cache = None       # LFS
+        self.TodoList = []   # List of TodoSections Objects IDs
+        self.starred = False
+
+        self.tags = []          # List of Strings
+        self.source = []        # Is it from Medium? Is it from Youtube
+        self.kind = []          # Is this a video, article, or image? What kind of content this represents
+        self.useCases = []      # List of UseCase Objects IDs
+        self.topics = []        # List of Topics that the Chip's contents fall into
+        self.phaseID = None
+        self.status = None      # 4-State Value: Done - In_Progress - ToDo - None(N/A)
+
+        self.collectionIDs = None
+        self.folderID = None
+
+    def inherit(self, p_bm):
+        self.name = p_bm.name
+        self.url = p_bm.url
+        self.time = p_bm.time
+
+    def serialize(self):
+        return  {
+                    "ID" : self.ID,
+                    "name" : self.name,
+                    "url" : self.url,
+                    "time" : self.time,
+                    "description" : self.description,
+                    "starred" : self.starred,
+                    "tags" : self.tags,
+                    "source" : self.source,
+                    "kind" : self.kind,
+                    "UseCases" : self.useCases,
+                    "topics" : self.topics,
+                    "phaseID" : self.phaseID,
+                    "status" : self.status,
+                }
+
+
+
+
+
+
+
+
+
+
+def generatePrimitiveBookmarks(limit):
+    for i in range(limit):
+        p_bm = Primitive_Bookmark()
+        p_bm.name = "Test_" + str(i)
+        p_bm.url = "https://www.test" + str(i) + ".com"
+        p_bm.time = "DateTimeObject_" + str(i)
+        yield p_bm
+
+def test_inherit(limit, show=False):
+    print("Testing Chip.inherit()")
+    try:
+        for p_bm in generatePrimitiveBookmarks(limit):
+            chip = Chip(p_bm)
+            # chip.inherit(p_bm)
+            if show:
+                chip.show()
+
+        print("Initialization of "+ str(limit) +" chips: True")
+    except Exception as e:
+        print("Error encountered in test_inherit()")
+
+def test_serialize(limit, show=False):
+    print("Testing Chip.serialize()")
+    try:
+        for pbm in generatePrimitiveBookmarks(limit):
+            chip = Chip(pbm)
+            if show:
+                pp = pprint.PrettyPrinter(indent=4)
+                pp.pprint(chip.serialize())
+                print("Serialization of "+ str(limit) +" chips: True")
+
+    except Exception as e:
+        print("Error encountered in test_serialize()")
+
+def test_(limit):
+    with open("data/chips.json", "w") as fout:
+        for pbm in generatePrimitiveBookmarks(limit):
+            chip = Chip(pbm)
+            json.dump(chip, fout, default=JSON.encoder)
+
+
 class UseCase(object):
     """
         docstring for UseCase
         ID Format: UC<USERCODE>XXXXX
     """
     def __init__(self):
-        self.ID = None          # NoneID invalidates the object, equivalent to deleting it
+        self.ID = "UC" + USERCODE + str(uuid.uuid4()).replace("-", "")          # NoneID invalidates the object, equivalent to deleting it
+        self.name = None
+        self.desc = None
+        self.projectIDs = []    # Projects that this UseCase is useful in
+        self.phaseID = None     # Phase that this UseCase is useful for
         pass
-
 
 
 class Todo(object):
@@ -40,7 +150,7 @@ class Todo(object):
         ID Format: T<USERCODE>XXXXX
     """
     def __init__(self):
-        self.ID = None          # NoneID invalidates the object, equivalent to deleting it
+        self.ID = "T" + USERCODE + str(uuid.uuid4()).replace("-", "")          # NoneID invalidates the object, equivalent to deleting it
         self.title = ""
         self.note = ""
         self.status = False
@@ -53,71 +163,14 @@ class TodoSection(object):
         ID Format: TS<USERCODE>XXXXX
     """
     def __init__(self):
-        self.ID = None          # NoneID invalidates the object, equivalent to deleting it
+        self.ID = "TS" + USERCODE + str(uuid.uuid4()).replace("-", "")          # NoneID invalidates the object, equivalent to deleting it
         self.heading = ""
         self.description = ""
         self.list = []      # List of Todo Objects IDs
         pass
 
-"""
-    ID : Unique ID of the Chip Object
-    img : Web Scraped image related to the chip, extacted from the url
-"""
-
-class Chip(Primitive_Bookmark):
-    """
-        docstring for Chip
-        ID Format: C<USERCODE>XXXXX
-    """
-    def __init__(self):
-        super()
-        self.ID = None          # NoneID invalidates the chip, equivalent to deleting it
-        self.description = ""
-        self.img = None         # LFS
-        self.cache = None       # LFS
-        self.TodoList = []   # List of TodoSections Objects IDs
-        self.starred = False
-
-        self.source = []        # Is it from Medium? Is it from Youtube
-        self.kind = []          # Is this a video, article, or image? What kind of content this represents
-        self.tags = []          # List of Strings
-        self.useCases = []      # List of UseCase Objects IDs
-        self.topics = []        # List of Topics that the Chip's contents fall into
-        self.phaseID = None     # Color Hex Value
-        self.status = None      # 4-State Value: Done - In_Progress - ToDo - None(N/A)
-
-        self.collectionIDs = None
-        self.folderID = None
-
-    def inherit(self, p_bm):
-        self.name = p_bm.name
-        self.url = p_bm.url
-        self.time = p_bm.time
-
-
-def generatePrimitiveBookmarks(limit):
-    for i in range(limit):
-        p_bm = Primitive_Bookmark()
-        p_bm.name = "Test_" + str(i)
-        p_bm.url = "https://www.test" + str(i) + ".com"
-        p_bm.time = "DateTimeObject_" + str(i)
-        yield p_bm
-
-def test_inherit(limit, show=False):
-    try:
-        for p_bm in generatePrimitiveBookmarks(limit):
-            chip = Chip()
-            chip.inherit(p_bm)
-            if show:
-                chip.show()
-
-        print("Testing Chip.inherit()")
-        print("Initialization of "+ str(limit) +" chips: True")
-    except Exception as e:
-        print("Error encountered in test_initPrimitiveBookmarks()")
-
 
 if __name__ == '__main__':
     colorama.init(autoreset = True)
     print(colorama.Fore.WHITE + colorama.Back.RED + 'Warning! This script is to be run internally by ' + PROJECT_NAME + ' scripts, direct use might lead to unexpected behaviour\n')
-    test_inherit(10000)
+    # test_inherit(10000)
