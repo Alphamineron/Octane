05553c3781062509f6fbfbd7712d6f77ad1d24f0
Alphamineron <alphamineron@gmail.com>
2019-07-23 02:25:42 +0530

    Primitive Search Implemented

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,3 +1,5 @@
+// let _ = require(['../node_modules/lunr/lunr']);
+
 if (document.readyState == "loading")
 	document.addEventListener("DOMContentLoaded", ready);
 else ready();
@@ -8,37 +10,123 @@ else ready();
 // =================================================================
 
 let chips = [];
-// const endpoint = "../../data/chips.json";
-const endpoint = "res/people.json";   // (Path Relative to html files sourcing this script)
-fetch(endpoint)
-    .then(response => response.json())
-    .then(function(data) {
-        chips.push(...data);
-        ready();
-    })
-    .catch(err => console.log(err));
-
+let recentSearches = [];
+let idx;
 
 function ready() {
+	const endpoint = "../../data/chips.json";
+	// const endpoint = "res/people.json";   // (Path Relative to html files sourcing this script)
+	fetch(endpoint)
+	    .then(response => response.json())
+	    .then(function(data) {
+	        chips.push(...data);
+	        main();
+	    })
+	    .catch(err => console.log(err));
+
+}
+
+function main() {
     renderData(chips)
+
+	initSearch()
+	// Sticking Event Listeners
+	const searchInput = document.querySelector('.search-input');
+	searchInput.addEventListener("keyup", (event) => {   // if enter/return is pressed
+														if (event.keyCode === 13)
+															handleSearch(event);
+													});
+	const searchIcon = document.querySelector('.search svg');
+	searchIcon.addEventListener("click", handleSearch);
 }
 
 function renderData(data) {
   var mainContainer = document.getElementById("myData");
   for (var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
-    // div.innerHTML = `
-    //     <span>${data[i].source}</span>
-    //     <a href="${data[i].url}">${data[i].name}</a>
-    // `;
     div.innerHTML = `
-        ${data[i].firstName} ${data[i].lastName}
-    `
+        <span>${data[i].source}</span>
+        <a href="${data[i].url}">${data[i].name}</a>
+    `;
+    // div.innerHTML = `
+    //     ${data[i].firstName} ${data[i].lastName}
+    // `
     mainContainer.appendChild(div);
   }
 }
 
+// ===============================< Search >====================================
 
+function initSearch() {
+	idx = lunr(function () {
+					this.ref('ID');
+					this.field('name', { boost: 10 });
+					this.field('url');
+
+					chips.forEach(function (doc) {
+											this.add(doc)
+										}, this);
+				});
+}
+
+function getSearchQuery() {
+	return document.querySelector('.search-input').value;
+}
+
+function updateSearchHistory() {
+	const searchInput = document.querySelector('.search-input');
+	// searchInput.value = "";			// Clear the searchbar after search
+	const searchHistory = document.querySelector('.search-history');
+	searchHistory.value = "";	// Clear the search history
+
+	// Loop the recentSearches and add to History
+	recentSearches.forEach(function(index, value) {
+								var li = document.createElement("li");
+								li.setAttribute("class", "history-item");
+								li.addEventListener("click", (event) => {
+																			searchInput.value = recentSearches[index];
+																		});
+								li.innerText = value;
+								searchHistory.appendChild(li);
+							});
+}
+
+function renderSearchResults(resultChips) {
+	var mainContainer = document.getElementsByClassName("search-results")[0];
+	for (var i = 0; i < resultChips.length; i++) {
+		var div = document.createElement("div");  // Create HTML element to display a data item
+		div.innerHTML = `
+		  <!-- <span>${resultChips[i].source}</span> -->
+		  <a href="${resultChips[i].url}">${resultChips[i].name}</a>
+		`;
+		mainContainer.appendChild(div);  // Append each data item one after another
+	}
+}
+
+function handleSearch(event) {
+	const searchInput = document.querySelector('.search-input');
+	const query = searchInput.value;	// Fetch the Search Query
+
+	recentSearches.push(query);		// Store the recent query
+	document.getElementsByClassName("search-results")[0].innerHTML = '';  // Clear Previous Search Results
+
+
+	var results = idx.search(query);	// Perform Search Operation on index
+	var resultChips = [];
+	results.forEach(function(result) {	 // Fetch complete info on Search Results
+								chips.forEach(function(chip) {
+															if (chip["ID"] == result["ref"]) {
+																resultChips.push(chip);
+															}
+														});
+							});
+
+
+	renderSearchResults(resultChips);
+
+	console.log("query:", query);
+	// alert(resultChips);
+}
 
 
 
