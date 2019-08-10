7b59ebdc88262f30446471b187de036b5eac826c
Alphamineron <alphamineron@gmail.com>
2019-08-11 01:28:51 +0530

    Add New Tree View (First Implementation)

---
+++ FrontEnd/preliminary/dist/js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+  addTreeEventListeners();
+});
+// =================================================================
+// 			Above Code Ensures that the JS isn't executed
+// 			In case the HTML hasn't been loaded as we are
+// 			Using async
+// =================================================================
+
+
+function uuidv4() {
+  return ([1e7]+1e3+4e3+8e3+1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  )
+}
+
+
+function addTreeEventListeners() {
+  // Event Listner to Catch the newly typed Foldername by User
+  $(".tree_item span[contenteditable]").blur(function(e) {
+    console.log($(this).text());
+  });
+
+  // Event Listener to control open/closing of folder branches in Tree
+  $('.tree_item').click(function (e) {
+    $(this).next().toggle(300);   // This is where the main magic happens
+
+    $(this).toggleClass('tree_item--opened');   // CSS Class that serves as a flag
+    if ($(this).hasClass('tree_item--opened')) {    // A flag for this shit
+      $(this).children('.tree_icon')                // Just changing the icon
+              .addClass('fa-folder-open')
+              .removeClass('fa-folder');
+    } else {
+      $(this).children('.tree_icon')
+              .addClass('fa-folder')
+              .removeClass('fa-folder-open');
+    }
+  });
+
+
+  // This part just make sure to render a selected node (Active Node)
+  // This can be modified to add multi-select abilities + Drag&Drop
+  // :has(ul) searches all the child elements of the found element for a <ul>
+  $('.tree_li').not(':has(ul, .tree_item--addBtn)').find('.tree_item').click(function (e) {
+    $('.tree li')   // First, Remove the Active State of any other .tree_item
+                .find('.tree_item')
+                .not(this)
+                .removeClass('tree_item--active');
+    // Finally, Toggle the active state of the Target .tree_item element
+    $(this).toggleClass('tree_item--active');
+  })
+
+
+  // Add Item Button Click Event Listener
+  $(".tree_item--addBtn").click(function() {
+    $(this).parent().before(`
+      <li class="tree_li tree_parent" id="${"F0000" + uuidv4()}">
+        <span class="tree_item">
+          <i class="fa fa-folder tree_icon" aria-hidden="true"></i>
+          <span style="outline:none;" contenteditable> New Folder </span>
+        </span>
+        <ul class="tree_child-list">
+          <li class="tree_li">
+            <span class="tree_item tree_item--addBtn">
+              <button class="fa fa-plus-circle"></button>
+            </span>
+          </li>
+        </ul>
+      </li>
+    `);
+
+    $(".tree").add('*').off("click blur"); // Drop all Event Listeners connected to the Tree
+    addTreeEventListeners();   // Call function again to add elistners to newly added folder
+  });
+}
---
+++ FrontEnd/preliminary/dist/js/treePopulate.js
@@ -0,0 +1,77 @@
+if (document.readyState == "loading")
+	document.addEventListener("DOMContentLoaded", ready);
+else ready();
+// =================================================================
+// 			Above Code Ensures that the JS isn't executed
+// 			In case the HTML hasn't been loaded as we are
+// 			Using async
+// =================================================================
+
+let folders;   // Global Variable
+
+function ready() {
+	const endpoint = "../../../data/folderTree.json";
+	fetch(endpoint)
+	    .then(response => response.json())
+	    .then(function(data) {
+	        folders = data;
+	        main();
+	    })
+	    .catch(err => console.log(err));
+}
+
+function returnTreeHTML(folder) {
+	// I don't like putting large strings in my funcs' logic, so I make a func
+	// for the strings... <Big Brain Time>
+    return `
+        <li class="tree_li tree_parent" id="${folder["id"]}">
+          <span class="tree_item">
+            <i class="fa fa-folder tree_icon" aria-hidden="true"></i>
+            <span style="outline:none;" contenteditable> ${folder["name"]} </span>
+          </span>
+          <ul class="tree_child-list">
+            <li class="tree_li">
+              <span class="tree_item tree_item--addBtn">
+                <button class="fa fa-plus-circle"></button>
+              </span>
+            </li>
+          </ul>
+        </li>
+    `;
+}
+
+function populateTree(ele, parentFolderList = folders) {
+  // Remove the "Add Item" btn, so we can append data to the tree in the
+  // right order without having the "Add Item" btn be the first item in tree
+  var li_addBtn = ele[0].querySelector("li.tree_li > .tree_item--addBtn").parentNode
+  li_addBtn.remove();
+
+  // Loop through the current level in the folder dictionary/tree and append it in HTML
+  parentFolderList.forEach(dict => {
+    folder = {
+      "id": dict["id"],
+      "name": dict["name"],
+      "children": dict["children"]
+    }
+    ele.append(returnTreeHTML(folder));
+
+	// If "children" exists then go recursive by selecting the child list
+    if (Array.isArray(folder["children"]) && folder["children"].length) { // children exists and is not empty
+      var $childFolders = ele.children("#" + folder["id"]).children("ul.tree_child-list");
+      populateTree($childFolders, folder["children"]);
+    }
+  });
+
+  // Add the "Add Item" btn again after being done appending all folders in the
+  // current level, in the end of the current level within the tree as much
+  // deep we might be in the tree(as we are using recursion)
+  ele[0].append(li_addBtn);
+}
+
+function main() {
+    var $tree = $("#tree");	  // If you need this comment, You gotta learn jQuery bud (At least Google it)...
+    populateTree($tree);	// Sort of Obvious what this func does
+
+    $(".tree").add('*').off();	// Clean Tree's EListeners, to avoid attaching multiple EListeners to the same element
+    addTreeEventListeners();	// Attach EListeners to the Tree's elements
+}
